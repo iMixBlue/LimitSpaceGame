e9cb8c3446934e1548d4828a8f236a01f3ac4689
MMXVIII404 <han1999@outlook.com>
2023-12-05 01:59:41 +0000

    Added portal logic
    Not finished yet

---
+++ Assets/Resources/Scripts/PortalTextureSetup.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalTextureSetup : MonoBehaviour
+{
+    public Camera cameraA;
+    public Camera cameraB;
+    public Material cameraAMat;
+    public Material cameraBMat;
+    void Start()
+    {
+        if (cameraA.targetTexture != null)
+        {
+            cameraA.targetTexture.Release();
+        }
+        cameraA.targetTexture = new RenderTexture(Screen.width, Screen.height, 24);
+        cameraAMat.mainTexture = cameraA.targetTexture;
+
+        if ( cameraB.targetTexture != null )
+        {
+            cameraB.targetTexture.Release();
+        }
+        cameraB.targetTexture = new RenderTexture(Screen.width, Screen.height, 24);
+        cameraBMat.mainTexture = cameraB.targetTexture;
+    }
+}
