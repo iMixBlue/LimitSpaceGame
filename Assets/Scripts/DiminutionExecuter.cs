ada4aae010419055b794144f93065a7d03e3d309
MMXVIII404 <han1999@outlook.com>
2023-12-22 09:31:05 +0000

    added

--- Assets/Scripts/DiminutionExecuter.cs
+++ Assets/Scripts/DiminutionExecuter.cs
@@ -15,13 +15,18 @@ public class DiminutionExecuter : Usable
     Character character;
 
 
-    public DiminutionExecuter(GameObject player) { _player = player; }
-    public void Use()
-    {
-        _currentScale = _player.transform.localScale;
-        Debug.Log("Using diminution potion!");
+    public DiminutionExecuter(GameObject player) {
+        _player = player;
         thirdPersonController = _player.GetComponent<ThirdPersonController>();
         character = _player.GetComponent<Character>();
+    }
+    public void Use()
+    {
+
+
+        _currentScale = _player.transform.localScale;
+        Debug.Log("Using diminution potion!");
+        
 
 
         // Change to cutscene animation
@@ -29,6 +34,7 @@ public class DiminutionExecuter : Usable
         {
             Debug.Log("Diminuting!");
             _player.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
+<<<<<<< Updated upstream
             _player.tag = "SmallPlayer";
             thirdPersonController.JumpHeight = 0.4f;
             thirdPersonController.MoveSpeed = 1.2f;
@@ -36,13 +42,26 @@ public class DiminutionExecuter : Usable
             thirdPersonController._animator.runtimeAnimatorController = character.newController;
             character.virtualCamera.GetCinemachineComponent<Cinemachine3rdPersonFollow>().ShoulderOffset.y = -0.35f;
             character.virtualCamera.GetCinemachineComponent<Cinemachine3rdPersonFollow>().CameraDistance = 0.5f;
+=======
+            SetMiniDetail();
+>>>>>>> Stashed changes
         }
         else
         {
             Debug.Log("Revovering!");
             _player.transform.localScale = Vector3.one;
-            // Change the position of main camera
+            // Change back to normal physic set
         }
+    }
+
+    public void SetMiniDetail()
+    {
+        thirdPersonController.MoveSpeed = 0.2f;
+        thirdPersonController.SprintSpeed = 0.5335f;
+        thirdPersonController.JumpHeight = 0.12f;
+        thirdPersonController.Gravity = -1.5f;
+
+        thirdPersonController._animator.runtimeAnimatorController = character.miniController;
     }
 }
 
